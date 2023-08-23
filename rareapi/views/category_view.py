3bc6681982ad8df3c96f053c178fbc4f61131f39
Katharine Zarbock <kjzarbock@gmail.com>
2023-08-23 00:05:51 -0500

    8/25 1515, added category view

---
+++ rareapi/views/category_view.py
@@ -0,0 +1,56 @@
+"""View module for handling requests about game types"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rareapi.models import Category
+
+
+class CategoryView(ViewSet):
+    """Level up game types view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single game type
+        Returns:
+            Response -- JSON serialized game type
+        """
+        category = Category.objects.get(pk=pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def list(self, request):
+        """Handle GET requests to get all game types
+
+        Returns:
+            Response -- JSON serialized list of game types
+        """
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        category = Category.objects.create(
+            label=request.data["label"],
+        )
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    
+    def update(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        category.label = request.data["label"]
+        category.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+    
+    def destroy(self, request, pk):
+        category = Category.objects.get(pk=pk)
+        category.delete()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+        
+
+class CategorySerializer(serializers.ModelSerializer):
+    """JSON serializer for customers"""
+
+    class Meta:
+        model = Category
+        fields = ('id', 'label')
