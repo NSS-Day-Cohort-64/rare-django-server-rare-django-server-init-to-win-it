5f8c78c7b93c5b25445948d8208df2d90a3251c5
Logan Welch <loganewelch@gmail.com>
2023-08-28 11:24:11 -0500

    resaved

--- rare/urls.py
+++ rare/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from rest_framework import routers
+from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 from rareapi.views import TagView
@@ -23,4 +25,5 @@ router.register(r'tags', TagView, 'tag')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
